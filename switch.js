012c3b6cae328a9dce8ef1fdaf190ded77c8e0f4
K M Marziul Karim <marziulrafi@gmail.com>
2025-02-08 20:08:14 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,19 @@
+var theme = "dark"
+
+switch(theme)
+{
+    case "light" :
+    console.log("Light theme applied")
+    break;
+
+    case "dark" : {
+    console.log("Dark theme applied")
+    break;
+    }
+    case "blue" :
+    console.log("Blue theme applied")
+    break;
+
+    default :
+    console.log("System theme applied")
+}
